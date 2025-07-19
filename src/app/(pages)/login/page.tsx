a4823437f2862396c46d959aaf6055d40467bea4
Julio <media.julio68@gmail.com>
2025-07-19 11:21:33 +0700

    Temporary-sidebar-v1

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,8 +1,6 @@
 export default function LoginPage() {
     return (
-        <main>
-            <h1>Login</h1>
-            <p>This is Login Page</p>
-        </main>
+        <h1>AHALAOOSAO</h1>
+        
     );
 }
